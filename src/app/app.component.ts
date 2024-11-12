226483eb91fc99b7ca8881c9e7627ea5b139ac5a
LeelaNarasimha <LeelaNarasimha@teamvelocitymarketing.com>
2024-11-12 08:34:18 +0530

    aasdasdsad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostListComponent],
+  imports: [RouterOutlet, PostListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular18-http';
+  private http = inject(HttpClient);
+  data: any;
+
+  fetchData() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .subscribe((response) => {
+        this.data = response;
+      });
+  }
 }
