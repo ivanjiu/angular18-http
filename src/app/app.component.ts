e035dcbb1811138a47817a6ce7178950a785f6be
LeelaNarasimha <LeelaNarasimha@teamvelocitymarketing.com>
2024-11-20 21:16:04 +0530

    ewewe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { DataFetcherComponent } from './data-fetcher/data-fetcher.component';
+import { CustomHttpParameterCodec } from './services/CustomHttpParameterCodec';
 
 @Component({
   selector: 'app-root',
@@ -22,11 +23,26 @@ export class AppComponent {
   private http = inject(HttpClient);
   data: any;
 
-  fetchData() {
-    this.http
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .subscribe((response) => {
-        this.data = response;
-      });
+  ngOnInit() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+    let httpParams = new HttpParams({
+      encoder: new CustomHttpParameterCodec(),
+    });
+    httpParams = httpParams.set('id', 1);
+    httpParams = httpParams.set('user Id', 1);
+
+    this.http.get(url, { params: httpParams }).subscribe((response) => {
+      this.data = response;
+    });
+
+    let decodeUrl = '/api/config?filter=ALL_POSTS&details=enabled_features';
+    const params = new HttpParams({
+      fromString: decodeUrl.split('?')[1],
+      encoder: new CustomHttpParameterCodec(),
+    });
+
+    console.log(params.get('filter'));
+    console.log(params.get('details'));
   }
 }
